6d2318948b937c47acc3eed2380467891f6a3a5e
ShiroiSen <ubaid.namaku@gmail.com>
2024-04-11 02:08:28 +0700

    advanced admin role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
- //
+        Gate::define('admin', function(User $user) {
+            $validRoles = ['admin', 'superAdmin'];
+            return $user->role && in_array($user->role, $validRoles);
+        });
     }
+    
 }
